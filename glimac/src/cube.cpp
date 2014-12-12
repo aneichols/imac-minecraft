4b9ef778c19e076f54deff22db2c60ae0cac30a5
julietteTaka <juliettetuxbelin@gmail.com>
2014-12-12 11:16:34 +0100

    add all directories and makefile to compile only main

---
+++ glimac/src/cube.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include "glimac/common.hpp"
+#include "glimac/Cube.hpp"
+
+namespace glimac {
+
+
+    void Cube::build(GLfloat s) {
+        ShapeVertex vertex;
+        //tableau de positions
+
+        glm::vec3 pos[] = {
+            glm::vec3(-1.0, -1.0, -1.0),   glm::vec3(1.0, -1.0, -1.0),   glm::vec3(1.0, 1.0, -1.0),     // Face 1
+            glm::vec3(-1.0, -1.0, -1.0),   glm::vec3(-1.0, 1.0, -1.0),   glm::vec3(1.0, 1.0, -1.0),     // Face 1
+
+            glm::vec3(1.0, -1.0, 1.0),   glm::vec3(1.0, -1.0, -1.0),   glm::vec3(1.0, 1.0, -1.0),       // Face 2
+            glm::vec3(1.0, -1.0, 1.0),   glm::vec3(1.0, 1.0, 1.0),   glm::vec3(1.0, 1.0, -1.0),         // Face 2
+
+            glm::vec3(-1.0, -1.0, 1.0),   glm::vec3(1.0, -1.0, 1.0),   glm::vec3(1.0, -1.0, -1.0),      // Face 3
+            glm::vec3(-1.0, -1.0, 1.0),   glm::vec3(-1.0, -1.0, -1.0),   glm::vec3(1.0, -1.0, -1.0),    // Face 3
+
+            glm::vec3(-1.0, -1.0, 1.0),   glm::vec3(1.0, -1.0, 1.0),   glm::vec3(1.0, 1.0, 1.0),        // Face 4
+            glm::vec3(-1.0, -1.0, 1.0),   glm::vec3(-1.0, 1.0, 1.0),   glm::vec3(1.0, 1.0, 1.0),        // Face 4
+
+            glm::vec3(-1.0, -1.0, -1.0),   glm::vec3(-1.0, -1.0, 1.0),   glm::vec3(-1.0, 1.0, 1.0),     // Face 5
+            glm::vec3(-1.0, -1.0, -1.0),   glm::vec3(-1.0, 1.0, -1.0),   glm::vec3(-1.0, 1.0, 1.0),     // Face 5
+
+            glm::vec3(-1.0, 1.0, 1.0),   glm::vec3(1.0, 1.0, 1.0),   glm::vec3(1.0, 1.0, -1.0),         // Face 6
+            glm::vec3(-1.0, 1.0, 1.0),   glm::vec3(-1.0, 1.0, -1.0),   glm::vec3(1.0, 1.0, -1.0)
+        };      // Face 6
+
+        glm::vec3 normals[] = {
+             glm::vec3(0.0, 0.0, 1.0),
+             glm::vec3(0.0, 1.0, 0.0),
+             glm::vec3(0.0, 0.0, 1.0),
+             glm::vec3(0.0, 1.0, 0.0),
+             glm::vec3(1.0, 0.0, 0.0),
+             glm::vec3(0.0, 1.0, 0.0),
+        };
+
+        vertex.texCoords = glm::vec2(0,0);
+
+        for (int i = 0; i < 36; i++){
+            vertex.position = pos[i];
+            int j = i / 6;
+            vertex.normal = normals[j];
+            m_Vertices.push_back(vertex);
+        }
+    }
+}
